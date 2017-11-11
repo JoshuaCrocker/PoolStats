751d0d3b3dd52446c370701cd4e8475c11b91019
Joshua Crocker <joshua.crocker@hotmail.co.uk>
2017-11-11 17:54:26 +0000

    Added team unit test and Team endpoint

--- app/Team.php
+++ app/Team.php
@@ -4,7 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Team
+ * @package App
+ */
 class Team extends Model
 {
-    //
+    /**
+     * Get the URL endpoint for the Team Model
+     *
+     * @return string
+     */
+    public function endpoint()
+    {
+        return '/teams/' . $this->id;
+    }
 }
