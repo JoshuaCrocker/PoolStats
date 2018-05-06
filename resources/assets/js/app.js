17e01edad242d46c9b42a7e89ad3f56c18ce9534
Joshua Crocker <joshua.crocker@hotmail.co.uk>
2018-05-06 15:03:40 +0100

    Added date pickers

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,21 +1,17 @@
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * Load Bootstrap and Modernizr (just checking Input Types)
  */
 
 require('./bootstrap');
-
-window.Vue = require('vue');
+require('./modernizr');
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * Register date pickers using the Pikaday library
  */
+if (!Modernizr.inputtypes.date) {
+    var Pikaday = require('pikaday');
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+    $('input[type=date]').each(function(el) {
+        new Pikaday({ field: this });
+    });
+}
